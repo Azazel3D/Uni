19abd87d688514e67dec589092018df80c6489e3
Azazel3D <fergarpi005@gmail.com>
2026-03-25 14:52:51 -0300

    Miku terminada
    problema 1 hecho

---
+++ Ayudantia1/Ayudantia1/src/Miku.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Miku {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = 0;
+
+        int uem = 0;
+        do {
+            System.out.println();
+            System.out.println("Ingrese el UEM necesario");
+            System.out.print("> ");
+            try {
+                uem = Integer.valueOf(sc.nextLine());
+
+                if (uem < 0) {
+                    System.out.println("Error, El valor tiene que ser positivo");
+                } else {
+                    x++;
+                    
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error, ingrese un valor numerico");
+                x = 0;
+            }
+        }while (x == 0);
+        
+        int dias = 0;
+        int completo = 0;
+        int total = 0;
+        int uemBase = uem;
+
+        while (uem > 0) {
+            ++dias;
+            if (dias % 5 == 0) {
+                uem -= 80;
+                completo++;
+                total += 80;
+            }else {
+                uem -= 50;
+                completo++;
+                total += 50;
+            }
+            
+        }
+
+        System.out.println("Miku tardaria " + dias + " dias en tener " + total + " UEM, con " + completo + " completos, "+
+        "le sobrarian " + (total - uemBase) + " UEM de las " + uemBase + " necesarios");
+
+        sc.close();
+    }
+}
+
--- Taller 1/taller 1/src/Taller_1.java
+++
@@ -1,5 +0,0 @@
-public class Taller_1 {
-    public static void main(String[] args){
-        
-    }
-}
